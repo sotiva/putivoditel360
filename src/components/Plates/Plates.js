b8e5132e36665d8c68125d94aae929f9996a4c33
sotiva <sotiva@yandex.ru>
2022-08-16 21:10:37 +0500

    added plates

--- src/components/Plates/Plates.js
+++ src/components/Plates/Plates.js
@@ -1,10 +1,12 @@
 function Plate (){
+	const title = 'Гора Муелды';
+
     return(
         <div>
             <div class="billet">
 					 
 					<div class="billet__select">
-						<h1 class="billet__head">Гора Муелды</h1>
+						<h1 class="billet__head">{title}</h1>
 						
 						<div class="billet__btn">
 							<a href="./#" class="billet__btn_link"><span>Начать тур</span></a>
